2b147f55b46f01990af3b87c68fb73eab83cfc87
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-17 19:07:40 +0300

    add constructor to project

--- src/main/java/module1/model/Project.java
+++ src/main/java/module1/model/Project.java
@@ -1,6 +1,16 @@
 package module1.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Project {
     private Long id;
     private String name;
+
+
+    public Project(Project project) {
+        this(project.getId(), project.getName());
+    }
 }
--- src/main/java/module1/repository/impl/IProjectRepositoryImpl.java
+++ src/main/java/module1/repository/impl/IProjectRepositoryImpl.java
@@ -2,21 +2,38 @@ package module1.repository.impl;
 
 import module1.model.Project;
 import module1.repository.IProjectRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public class IProjectRepositoryImpl implements IProjectRepository {
     private List<Project> projects  = new ArrayList<>();
 
     @Override
     public Optional<Project> findById(Long id) {
-        return Optional.empty();
+        return projects.stream().filter(s -> s.getId() == id).findFirst();
     }
 
     @Override
     public Project save(Project project) {
-        return null;
+        Project existingProject = projects
+                .stream()
+                .filter(s -> s.equals(project))
+                .findFirst().orElse(null);
+
+        if (existingProject == null) {
+            projects.add(project);
+
+            return project;
+        } else {
+            projects.remove(project);
+            projects.add(new Project(project));
+        }
+
+
+        projects.add(project);
+        return project;
     }
 }
