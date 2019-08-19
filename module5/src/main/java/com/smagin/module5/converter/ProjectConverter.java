4acc12f4066c305c62222d260261d4e8a91218e5
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-19 11:37:56 +0300

    add converter

--- module5/src/main/java/com/smagin/module5/converter/ProjectConverter.java
+++ module5/src/main/java/com/smagin/module5/converter/ProjectConverter.java
@@ -1,8 +1,19 @@
 package com.smagin.module5.converter;
 
+import com.smagin.module5.converter.api.Converter;
+import com.smagin.module5.dto.ProjectDto;
+import com.smagin.module5.model.Project;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ProjectConverter {
+public class ProjectConverter implements Converter<Project, ProjectDto> {
 
+    @Override
+    public ProjectDto convert(Project project) {
+        ProjectDto dto = new ProjectDto();
+        dto.setId(project.getId());
+        dto.setName(project.getName());
+        dto.setDate(project.getDate());
+        return dto;
+    }
 }
